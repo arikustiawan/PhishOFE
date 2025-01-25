88cb3dbc7bdf54cb37b086ebc3a541cece95e13a
kodthink <1221404256@student.mmu.edu.my>
2025-01-25 14:09:57 +0800

    Update feature.py

--- feature.py
+++ feature.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 from urllib.parse import urlparse
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+import numpy as np
 #import ipaddress
 #import socket
 import requests
