9eeb4de268909f597579e4866f5eb9149de8971f
kodthink <1221404256@student.mmu.edu.my>
2025-01-26 08:46:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-# Configure the page to remove the default header and footer
-st.set_page_config(
-    page_title="Your App Title",
-    page_icon="🏠",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-# Hide the Streamlit header, footer, and menu
-hide_menu_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-"""
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 # 1. as sidebar menu
 with st.sidebar:
     selected = option_menu(
         menu_title=None,  # required
         options=["Predict URL", "Projects", "Contact"],  # required
         icons=["house", "book", "envelope"],  # optional
+        default_index=0
     )
 
 if selected == "Predict URL":
-    st.title(f"You have selected {selected}")
+    #st.title(f"You have selected {selected}")
+    exec(open("pages/Predict_URL.py").read())
 if selected == "Projects":
     st.title(f"You have selected {selected}")
 if selected == "Contact":
