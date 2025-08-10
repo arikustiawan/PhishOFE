3340cb23fbd755b37021f81ebadee97ebebdd35c
arikustiawan <ariekustiawan@gmail.com>
2025-08-10 13:41:27 +0800

    Update Predict_URL.py

--- Predict_URL.py
+++ Predict_URL.py
@@ -74,8 +74,8 @@ if st.button("Check URL"):
             y = model.predict(x)
             #st.write("predict: ",y)
 
-            y_prob_phishing = model.predict_proba(x)[0,1]
-            y_prob_non_phishing = model.predict_proba(x)[0,0]
+            y_prob_phishing = model.predict_proba(y)[0,1]
+            y_prob_non_phishing = model.predict_proba(y)[0,0]
             st.write(y_prob_phishing)
             st.write(y_prob_non_phishing)
             
